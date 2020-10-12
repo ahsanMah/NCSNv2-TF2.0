6384556de137dd89363bcab08014ee342ef21172
Ahsan Mahmood <amahmood@theia.ia.unc.edu>
2020-10-12 12:16:35 -0400

    - Initial dump - Layers do not use sigmas - Two versions of RefineNet (v1, base) - Train and generate modified to pass sigmas to model instead of indexes - Some outdated functionality present to allow for pixel-level conditioning

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+import tensorflow as tf
+
+import configs
+import evaluation
+from generating import inpainting, generate, k_nearest, intermediate
+import trainv2 as train
+import utils
+
+import celeb_a_statistics
+
+utils.manage_gpu_memory_usage()
+
+EXPERIMENTS = {
+    "train": train.main,
+    "generate": generate.main,
+    "inpainting": inpainting.main,
+    "evaluation": evaluation.main,
+    "k_nearest": k_nearest.main,
+    "intermediate": intermediate.main,
+    "celeb_a_statistics": celeb_a_statistics.main
+}
+
+if __name__ == '__main__':
+    tf.get_logger().setLevel('ERROR')
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    args = utils.get_command_line_args()
+    configs.config_values = args
+
+    run = EXPERIMENTS[args.experiment]
+
+    run()
